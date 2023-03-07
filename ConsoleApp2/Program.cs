8f195337d0dc2bf7c2dca5a318cbfe3cd9dfecd4
jasmi <jasmi@MSI>
2023-03-07 10:57:09 +0800

    加入專案檔案。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("BMI:");
+            Console.WriteLine(Math.Round(48/(1.6*1.6),2));
+        }
+    }
+}
